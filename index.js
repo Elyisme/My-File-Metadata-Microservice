5f3be25e506a6260d48f604c3eb3cb772b446642
Elie Hayek <eliyahayek@gmail.com>
2024-08-16 18:53:10 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require('multer');
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// Set up multer for file handling
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Handle file upload and metadata extraction
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+
+  var file = req.file;
+  var fileSize = file.size;
+  var fileName = file.originalname;
+  var fileType = file.mimetype;
+
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize
+  });
+});
 
 
 
